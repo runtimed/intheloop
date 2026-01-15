4835396fb5ab36ed49c1ceeaa1d9a4dcfd05bab5
JacobRoberts <jroberts@anaconda.com>
2026-01-15 10:18:12 -0800

    format

--- backend/clients/projects-client.ts
+++ backend/clients/projects-client.ts
@@ -212,7 +212,7 @@ export class ProjectsClient {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${this.config.bearerToken}`,
-            "User-Agent": "intheloop"
+            "User-Agent": "intheloop",
           },
           body: body ? JSON.stringify(body) : undefined,
         });
